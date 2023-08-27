325f13e4b23c2e60623049b51e6b582428018e6b
Md. Saidul Islam <saidulislam4d@gmail.com>
2023-08-27 21:20:08 +0600

    Step#5: Replace string literals with constants, storing them in Constants.java

---
+++ Constants.java
@@ -0,0 +1,14 @@
+public class Constants{
+	public static String FILE_NAME = "students.txt";
+	public static String LOADING = "Loading data ...";
+	public static String LOADED = "Data Loaded"; 	
+	public static String FOUND = "We found it!"; 
+	public static String DATE_FORMAT = "dd/mm/yyyy-hh:mm:ss a";	
+	public static String WRONG_ARGUMENT_COUNT = "Number of Argument should be 1";
+	public static String SEPARATOR = ", ";
+	public static String ALL_NAMES = "a";
+	public static String RANDOM_NAME = "r";
+	public static String ADD_NAME = "+";
+	public static String QUERY = "?";
+	public static String COUNT = "c";
+}
--- StudentList.java
+++ StudentList.java
@@ -6,64 +6,64 @@ public class StudentList {
 	public static String line;
 	public static void reader(){
 		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Constants.FILE_NAME))); 
 			line = reader.readLine();
-			names = line.split(", ");			
+			names = line.split(Constants.SEPARATOR);			
 		} catch (Exception e){} 
 	}
 	public static void writer(String newName){
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter("students.txt", true));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(Constants.FILE_NAME, true));
 			Date datetime = new Date();
-			String dateformat = "dd/mm/yyyy-hh:mm:ss a";
+			String dateformat = Constants.DATE_FORMAT;
 			DateFormat dateFormat = new SimpleDateFormat(dateformat);
 			String formatdate = dateFormat.format(datetime);
-			writer.write(", " + newName + "\nList last updated on " + formatdate);
+			writer.write(Constants.SEPARATOR + newName + "\nList last updated on " + formatdate);
 			writer.close();
 		} catch (Exception e){}
 	}
 	public static void main(String[] args) {
 		if(args.length != 1){
-			System.out.println("Number of Argument should be 1");
+			System.out.println(Constants.WRONG_ARGUMENT_COUNT);
 		}
 //		Check arguments
-		else if(args[0].equals("a")) {
-			System.out.println("Loading data ...");		
+		else if(args[0].equals(Constants.ALL_NAMES)) {
+			System.out.println(Constants.LOADING);		
 			reader();	
 			for(String name : names) { 
 				System.out.println(name); 
 			}
-			System.out.println("Data Loaded.");
+			System.out.println(Constants.LOADED);
 		}
-		else if(args[0].equals("r")) {
-			System.out.println("Loading data ...");			
+		else if(args[0].equals(Constants.RANDOM_NAME)) {
+			System.out.println(Constants.LOADING);			
 			reader();
 			Random random = new Random();
 			int randomIndx = random.nextInt(names.length);
 			System.out.println(names[randomIndx]);
-			System.out.println("Data Loaded.");			
+			System.out.println(Constants.LOADED);			
 		}
-		else if(args[0].contains("+")){
-			System.out.println("Loading data ...");			
+		else if(args[0].contains(Constants.ADD_NAME)){
+			System.out.println(Constants.LOADING);			
 			String newName = args[0].substring(1);
 			writer(newName);
-			System.out.println("Data Loaded.");	
+			System.out.println(Constants.LOADED);	
 		}
-		else if(args[0].contains("?")) {
-			System.out.println("Loading data ...");		
+		else if(args[0].contains(Constants.QUERY)) {
+			System.out.println(Constants.LOADING);		
 			reader();	
 			boolean done = false;
 			String query = args[0].substring(1);
 			for(int index = 0; index<names.length && !done; index++) {
 				if(names[index].equals(query)) {
-					System.out.println("We found it!");
+					System.out.println(Constants.FOUND);
 					done=true;
 				}
 			}
-			System.out.println("Data Loaded.");			
+			System.out.println(Constants.LOADED);			
 		}
-		else if(args[0].contains("c")) {
-			System.out.println("Loading data ...");	
+		else if(args[0].contains(Constants.COUNT)) {
+			System.out.println(Constants.LOADING);	
 			reader();
 			char characters[] = line.toCharArray();			
 			boolean in_word = false;
@@ -80,7 +80,7 @@ public class StudentList {
 				}
 			}
 			System.out.println(count + " word(s) found ");
-			System.out.println("Data Loaded.");						
+			System.out.println(Constants.LOADED);						
 		}
 	}
 }
